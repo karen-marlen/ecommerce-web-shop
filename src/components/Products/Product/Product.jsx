b3a6ee299e6d6c461d4df9dd0fc48759f7542b27
Karen Marlen <72042911+karen-marlen@users.noreply.github.com>
2020-11-30 11:23:35 -0300

    styles-cart

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -2,32 +2,34 @@ import React from 'react';
 import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons';
 
-import useStyle from './styles';
+import useStyles from './styles';
 
-const Product = ({ product }) => {
-    const classes = useStyles();
+const Product = ({ product, onAddToCart }) => {
+  const classes = useStyles();
 
-    return (
-        <Card className={classes.root}>
-            <CardMedia className={classes.media} image={product.media.source} title={product.name} />
-            <CardContent>
-                <div className={classes.cardContent}>
-                    <Typography variant="h5" gutterBottom>
-                        {product.name}
-                    </Typography>
-                    <Typography variant="h5">
-                        {product.price.formatted_with_symbol}
-                    </Typography>
-                </div>
-                <Typography variant="body2" color="textSecondary">{product.description}</Typography>
-            </CardContent>
-            <CardActions disableSpacing className={classes.cardActions}>
-                <IconButton aria-label="Añadir al carrito">
-                    <AddShoppingCart />
-                </IconButton>
-            </CardActions>
-        </Card>
-    )
-}
+  const handleAddToCart = () => onAddToCart(product.id, 1);
 
-export default Product
+  return (
+    <Card className={classes.root}>
+      <CardMedia className={classes.media} image={product.media.source} title={product.name} />
+      <CardContent>
+        <div className={classes.cardContent}>
+          <Typography gutterBottom variant="h5" component="h2">
+            {product.name}
+          </Typography>
+          <Typography gutterBottom variant="h5" component="h2">
+            ${product.price.formatted}
+          </Typography>
+        </div>
+        <Typography dangerouslySetInnerHTML={{ __html: product.description }} variant="body2" color="textSecondary" component="p" />
+      </CardContent>
+      <CardActions disableSpacing className={classes.cardActions}>
+        <IconButton aria-label="Add to Cart" onClick={handleAddToCart}>
+          <AddShoppingCart />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default Product;
